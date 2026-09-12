f3de12fac4ddc3965a46b350585a30510912e4e5
Anshul0x5 <Anshul.38213@stu.upes.ac.in>
2026-09-13 01:08:06 +0530

    Add files via upload

---
+++ code2day1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int num1, num2;
+    printf("enter first number:");
+    scanf("%d",&num1);
+    printf("enter second number:");
+    scanf("%d",&num2);
+    printf("sum=%d\n",num1+num2);
+    printf("diffrence=%d\n",num1-num2);
+    printf("product=%d\n",num1*num2);
+    printf("quotient=%d\n",num1/num2);
+    return 0;
+}
---
+++ sumof2numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    int b;
+    printf("enter b");
+    scanf("%d",&b);
+    printf("sum is: %d",a+b);
+    return 0;
+}
